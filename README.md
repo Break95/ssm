3bb6ae673007e166c1288352cc987ea3a2687cc4
Pablo Oliver <olivercortespablo@gmail.com>
2020-01-23 21:37:20 +0100

    Linear model advance

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# ssm
-Step by Step Models
+# S.S.M. - Step by Step Models
+Step by Step Models is a R Shiny Application designed to visualize how different machine learning models learn/evolve.
+
+Supported Models:
+* None
+
+Models under development: 
+1. Simple Linear Regression
+2. Multiple Linear Regression
+3. Logistic regression
+
+Available datasets:
+* None
--- app.R
+++ app.R
@@ -8,21 +8,27 @@
 #
 
 library(shiny)
-library(shinydashboard)
+library(shinydashboardPlus)
 
+library(dplyr)
 library(ggplot2)
 
 # Define UI
-ui <- dashboardPage(
-    dashboardHeader(
+ui <- dashboardPagePlus(
+    header = dashboardHeaderPlus(
+        
         # Application title
-        title = "Step by Step Models"
+        title = span(tagList(img(src = "logo_mecydes.png", height = '25px', width = '25px'), "Step by Step Models")),
+        
+        # Right side bar
+        enable_rightsidebar = TRUE,
+        rightSidebarIcon = "info-circle"
     ),
     dashboardSidebar(
         sidebarMenu(
-            menuItem("Simple Linear Regression", tabName="slr"),
-            menuItem("Multiple Linear Regression", tabName = "mlr"),
-            menuItem("Logistic Regression", tabName = "logr")
+            menuItem("Simple Linear Regression", tabName="slr", icon = icon("box")),
+            menuItem("Multiple Linear Regression", tabName = "mlr", icon = icon("boxes")),
+            menuItem("Logistic Regression", tabName = "logr", icon = icon("wave-sine"))
         )
     ),
     dashboardBody(
@@ -35,20 +41,51 @@ ui <- dashboardPage(
                     sidebarPanel(
                         selectInput("dataset", "Choose a dataset:", c('longley', 'BostonHousing', 'abelone')),
                         uiOutput("variable"),
-                        uiOutput("objective")
+                        uiOutput("objective"),
+                        fluidRow(
+                            column(3,
+                                   numericInput("theta0", "t0:", 0)),
+                            column(3,
+                                   numericInput("theta1", "t1:", 0))
+                        )
                     ),
                     
                     mainPanel(
                         plotOutput("plot")
                     )
-                )
+                ),
+                hr(),
+                actionButton("step", "Step"),
+                actionButton("slowSteps", "Slow Steps"),
+                actionButton("final", "Final"),
+                uiOutput("error")
             ),
+            
+            # Multi Linear Regression
             tabItem(tabName = "mlr",
                 h2("Multiple Linear Regression")
             ),
+            
+            # Logistic Regression
             tabItem(tabName = "logr",
                 h2("Logistic Regression")
             )
+        )
+    ),
+    rightsidebar = rightSidebar(
+        backgroud = "dark",
+        rightSidebarTabContent(
+            id = 1,
+            title = h3("About"),
+            icon = "address-card",
+            active = TRUE,
+            ("Información de la página, [...]")
+        ),
+        rightSidebarTabContent(
+            id = 2,
+            title = "Updates",
+            icon = "wrench",
+            textOutput("v0.1 ......")
         )
     )
 )
@@ -56,6 +93,7 @@ ui <- dashboardPage(
 # Define server logic
 server <- function(input, output, session) {
 
+    # Outputs
     output$variable <- renderUI({
         selectInput("variable1", "Select predictor variable:", names(get(input$dataset)))
     })
@@ -67,11 +105,40 @@ server <- function(input, output, session) {
     output$plot <- renderPlot({
         p <-ggplot(data = get(input$dataset), aes(x = get(input$variable1), y = get(input$variable2))) +
             geom_point() + 
+            geom_abline(intercept = input$theta0, slope = input$theta1) + 
             labs(title="Linear Regresion", x = input$variable1, y = input$variable2)
         
         print(p)
     })
+
+    # Observed Events
+    observeEvent(input$step, {
+        variable = input$variable1
+        data = get(input$dataset)
+        t0 = input$theta0
+        t1 =  input$theta1
+        
+        calc_error <- function(th0, th1, x){
+            test <- th0+(th1 * x)
+            test
+        }
+        
+        error <- data$variable %>% calc_error(t0, t1, .) %>% sum()
+        #error <- do(data, aggregate((t1 + t0 * .$variable)))
+        
+       print(error)
+    })
+    
+    observeEvent(input$slowSteps, {
+        
+    })
+    
+    observeEvent(input$final, {
+        
+    })
+    
 }
 
 # Run the application 
+shinyAppDir(".")
 shinyApp(ui = ui, server = server)
