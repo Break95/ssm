956a13f397934b89e9677c7479f223ab056298d4
Pablo Oliver Cortés <Pablo.Oliver@uclm.es>
2020-01-24 12:09:11 +0100

    Linear Regression fixes. Minor UI fixes.

--- app.R
+++ app.R
@@ -8,6 +8,7 @@
 #
 
 library(shiny)
+library(shinydashboard)
 library(shinydashboardPlus)
 
 library(dplyr)
@@ -43,10 +44,12 @@ ui <- dashboardPagePlus(
                         uiOutput("variable"),
                         uiOutput("objective"),
                         fluidRow(
-                            column(3,
+                            column(4,
                                    numericInput("theta0", "t0:", 0)),
-                            column(3,
-                                   numericInput("theta1", "t1:", 0))
+                            column(4,
+                                   numericInput("theta1", "t1:", 0)),
+                            column(4,
+                                   numericInput("learnRate", HTML("&alpha;:") , 0.05 ))
                         )
                     ),
                     
@@ -58,7 +61,7 @@ ui <- dashboardPagePlus(
                 actionButton("step", "Step"),
                 actionButton("slowSteps", "Slow Steps"),
                 actionButton("final", "Final"),
-                uiOutput("error")
+                textOutput("error")
             ),
             
             # Multi Linear Regression
@@ -110,33 +113,95 @@ server <- function(input, output, session) {
         
         print(p)
     })
-
-    # Observed Events
+    
+    
+    ############################
+    ###### Observed Events #####
+    ############################
+    
+    # Complete one step of gradient descent.
     observeEvent(input$step, {
-        variable = input$variable1
+        var1 = input$variable1
+        var2 = input$variable2
         data = get(input$dataset)
-        t0 = input$theta0
-        t1 =  input$theta1
+        th0 = input$theta0
+        th1 =  input$theta1
         
-        calc_error <- function(th0, th1, x){
-            test <- th0+(th1 * x)
-            test
+        # 
+        calc_error <- function(x){
+            pred <- th0 + (th1 * x[var1])
+            error <- pred - x[var2]
+            error
         }
         
-        error <- data$variable %>% calc_error(t0, t1, .) %>% sum()
-        #error <- do(data, aggregate((t1 + t0 * .$variable)))
         
-       print(error)
+        
+        output$error <- renderText({
+            toString(sum(apply(data, 1, calc_error)))
+        })
+        
+        # Theta update function
+        th1_error <- function(x){
+            pred <- th0 + (th1 * x[var1])
+            print(x[var1])
+            error <- (pred - x[var2]) * x[var1]
+            error
+        }
+        
+        lr <- input$learnRate
+        
+        # Update Thetas
+        newTheta0 <- th0 - (input$learnRate / nrow(data)) * sum(apply(data, 1, calc_error))
+        newTheta1 <- th1 - (input$learnRate / nrow(data)) * sum(apply(data, 1, th1_error) )
+        
+        updateNumericInput(session, "theta0", value = newTheta0)
+        updateNumericInput(session, "theta1", value = newTheta1)
     })
     
+    # Automatically reduce error, slow for visualization.
     observeEvent(input$slowSteps, {
         
     })
     
+    # Get fitted model through normal ecuation.
     observeEvent(input$final, {
         
     })
     
+    ############################
+    ###### Other functions #####
+    ############################
+    
+    # Normalizattion of the dataframe to range [-1,1]
+    variable_normalization <- function(dataset){
+        mins <- apply(dataset, 2, max_cols)
+        maxs <- apply(dataset, 2, min_cols)
+        means <- apply(dataset, 2, mean_cols)
+        
+        
+    }
+    
+    # Gets the maximun value of each column.
+    max_cols <- function(x){
+        max(x)
+    }
+    
+    # Gets the min value of each column.
+    min_cols <- function(x){
+        minx(x)
+    }
+    
+    # Gets the mean of each column.
+    mean_cols <- function(x){
+        mean(x)
+    }
+    
+    # Transform 
+    normaliza_cols <- function(x, min, max, mean){
+        for(i in 1:nrow(x)){
+            x[i] = (x[i] - mean) / (max - min)
+        }
+    }
 }
 
 # Run the application 
